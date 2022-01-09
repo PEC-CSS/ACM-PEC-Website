08a53ef7806841932007a4fce3871b771274fd60
HarshDutt17 <78022802+HarshDutt17@users.noreply.github.com>
2022-01-09 22:45:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch , Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
