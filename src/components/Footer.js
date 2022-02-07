5899eea1da13b49128cc827d3c236b21abf14069
jagjit <jagjitsingh.btcivil20@pec.edu.in>
2022-02-07 16:39:01 +0530

    Basic layout of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
+import "../assets/stylesheets/Footer.css";
+import {FaFacebookF,FaTwitter,FaInstagram,FaYoutube} from 'react-icons/fa';
 
 class Home extends Component {
     render() {
         return (
-                <div><h2>Footer</h2></div>
+                <div className='Footer '>
+                    <div className='Footer__HeadContainer '>
+                        <div className='Footer__Head spaced'>
+                            <div className='Footer__HeadTitle'>
+                                ACM-CSS
+                            </div>
+                            <div className='Footer__HeadSocialsContainer'>
+                                <div className='Footer__HeadTitle'>Socials:</div>
+                                <ul className='Footer__HeadSocials'>
+                                    <li><a><FaFacebookF className='facebookIcon' /></a></li>
+                                    <li><a><FaTwitter className='twitterIcon'/></a></li>
+                                    <li><a><FaInstagram  className='instagramIcon'/></a></li>
+                                    <li><a><FaYoutube className='youtubeIcon'/></a></li>
+                                </ul>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div className='Footer__MainContainer'>
+                        <div className='Footer__Main spaced'>
+                            <div className='Footer__MainCols'>
+                                about us
+                            </div>
+                            <div className='Footer__MainCols'>
+                                recent blogs
+                            </div>
+                            <div className='Footer__MainCols'>
+                                contact us
+                            </div>
+                            <div>
+
+                            </div>
+                        </div>  
+                    </div>
+                </div>
             )
     }
 }
