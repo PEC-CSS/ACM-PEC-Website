d0dfb6a543ac6308234954831d4a37bc0e63ce48
jagjit <jagjitsingh.btcivil20@pec.edu.in>
2022-02-08 12:26:07 +0530

    rebase

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import "../assets/stylesheets/Footer.css";
-import {FaFacebookF,FaLinkedin,FaInstagram,FaYoutube} from 'react-icons/fa';
-=======
->>>>>>> parent of 5899eea (Basic layout of footer)
 
 class Home extends Component {
     render() {
         return (
-<<<<<<< HEAD
-                <div className='Footer '>
-                    <div className='Footer__HeadContainer '>
-                        <div className='Footer__Head spaced'>
-                            <div className='Footer__HeadTitle'>
-                                ACM-CSS
-                            </div>
-                            <div className='Footer__HeadSocialsContainer'>
-                                <div className='Footer__HeadTitle'>Socials:</div>
-                                <ul className='Footer__HeadSocials'>
-                                    <li><a href='https://www.facebook.com/pecacm' target="new"><FaFacebookF className='facebookIcon' /></a></li>
-                                    <li><a href='https://www.linkedin.com/company/pec-acm-student-chapter/' target="new"><FaLinkedin className='linkediIcon'/></a></li>
-                                    <li><a href='https://instagram.com/pecacm' target="new"><FaInstagram  className='instagramIcon'/></a></li>
-                                    <li><a href='https://www.youtube.com/c/PECACMStudentChapter' target="new"><FaYoutube className='youtubeIcon'/></a></li>
-                                </ul>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    <div className='Footer__MainContainer'>
-                        <div className='Footer__Main spaced'>
-                            <div className='Footer__MainCols'>
-                                about us
-                            </div>
-                            <div className='Footer__MainCols'>
-                                recent blogs
-                            </div>
-                            <div className='Footer__MainCols'>
-                                contact us
-                            </div>
-                            <div>
-
-                            </div>
-                        </div>  
-                    </div>
-                </div>
-=======
                 <div><h2>Footer</h2></div>
->>>>>>> parent of 5899eea (Basic layout of footer)
             )
     }
 }
