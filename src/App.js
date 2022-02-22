037ce4f250bea83d606f9632b462890c5ffcbe51
Saiyamgupta <68195719+saiyam-gupta@users.noreply.github.com>
2022-02-22 23:30:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Body from './components/Body';
+import Body from './components/About-Us-Body';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
