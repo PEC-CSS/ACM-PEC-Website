ff78fd2cc3caafe479287c709e7e8294bc787630
Harsh Johar <hpsj3112@gmail.com>
2022-02-26 23:47:39 +0530

    events backend done

---
+++ acmwebsite/schemas/events.js
@@ -0,0 +1,58 @@
+export default {
+    name: "event",
+    title: "Event",
+    type: "document",
+    fields: [
+        {
+            name: "title",
+            title: "Title",
+            type: "string",
+        },
+        {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            options: {
+                source: "title",
+                maxLength: 96,
+            },
+        },
+        {
+            name: "mainImage",
+            title: "Main image",
+            type: "image",
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: "publishedAt",
+            title: "Published at",
+            type: "datetime",
+        },
+        {
+            name: "content",
+            title: "Content",
+            type: "string",
+        },
+        {
+            name: "body",
+            title: "Body",
+            type: "blockContent",
+        },
+    ],
+
+    preview: {
+        select: {
+            title: "title",
+            author: "author.name",
+            media: "mainImage",
+        },
+        prepare(selection) {
+            const { author } = selection;
+            return Object.assign({}, selection, {
+                subtitle: author && `by ${author}`,
+            });
+        },
+    },
+};
