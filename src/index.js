6632fc48a102ccae37c6d5e90bf740b46a0378f4
Gaurav Sharma <gauarvsharma.8699@gmail.com>
2022-02-28 16:38:01 +0530

    Minor Cleanup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import {Provider} from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
-import reducers from './utils/apis_redux/reducers';
+
 import './index.css';
+
 import App from './App';
+import reducers from './utils/apis_redux/reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||compose;
 const store = createStore(reducers,
   composeEnhancers(applyMiddleware(reduxThunk)));
 
+  
 ReactDOM.render(
   <Provider store={store}>
     <App />
